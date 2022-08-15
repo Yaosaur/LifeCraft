69cf165acab4fd12c0443aa157bd3909cf4e0e85
Yao Jiang <yaojiang7595@gmail.com>
2022-08-15 11:45:46 -0400

    Refactored views

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const ejsMate = require('ejs-mate');
 const path = require('path');
 const session = require('express-session');
 const methodOverride = require('method-override');
@@ -20,6 +21,7 @@ mongoose.connection.once('open', () => {
   console.log('connected to mongo');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
