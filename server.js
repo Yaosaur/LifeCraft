fdb3a914d4e2882d0637d7830422daeabe0ce034
Yao Jiang <yaojiang7595@gmail.com>
2022-08-15 22:53:12 -0400

    Filtering and searching on index functional

--- server.js
+++ server.js
@@ -66,8 +66,33 @@ app.get('/api/v1/', async (req, res) => {
 });
 
 app.get('/api/v1/crafts', async (req, res) => {
-  const crafts = await Craft.find({});
-  res.render('index', { crafts, craftSchema });
+  let crafts = undefined;
+  const { title } = req.query;
+  if (title) {
+    crafts = await Craft.find({ title: new RegExp(`${title}`, 'i') });
+  } else {
+    crafts = await Craft.find({});
+  }
+  res.render('index', { crafts, craftSchema, title });
+});
+
+app.get('/api/v1/crafts/filter', async (req, res) => {
+  const { category } = req.query;
+  const { price } = req.query;
+  if (category) {
+    let crafts = await Craft.find({ category: category });
+    res.render('index', { crafts, craftSchema, title: undefined });
+  } else if (price) {
+    let crafts = undefined;
+    if (price === 'lt5') {
+      crafts = await Craft.find({ price: { $lt: 5 } });
+    } else if (price === 'gt50') {
+      crafts = await Craft.find({ price: { $gt: 50 } });
+    } else {
+      crafts = await Craft.find({ price: { $gte: 5, $lte: 50 } });
+    }
+    res.render('index', { crafts, craftSchema, title: undefined });
+  }
 });
 
 app.post('/register', async (req, res, next) => {
