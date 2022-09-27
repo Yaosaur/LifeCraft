e1860791b03df40811b511f0d0a695646e1dfdc9
Yao Jiang <yaojiang7595@gmail.com>
2022-09-26 20:13:15 -0400

    Added confirmation modal

--- public/scripts/cartScript.js
+++ public/scripts/cartScript.js
@@ -1,11 +1,24 @@
 const selectors = document.querySelectorAll('.selector');
 const updaters = document.querySelectorAll('.updatebtn');
 const purchaseBtn = document.querySelector('#purchaseBtn');
+const confirmation = document.querySelector('#confirmation');
+const closeConfirmation = document.querySelector('#closeConfirmation');
 
 selectors.forEach((selector, index) =>
   selector.addEventListener('change', event => updaters[index].click())
 );
 
-purchaseBtn.addEventListener('click', event => {
-  axios.delete('http://localhost:3000/api/v1/cart/checkout');
+overlay.addEventListener('click', () => {
+  removeDisplay(confirmation);
+});
+
+purchaseBtn &&
+  purchaseBtn.addEventListener('click', event => {
+    showDisplay(overlay);
+    showDisplay(confirmation);
+    axios.delete('http://localhost:3000/api/v1/cart/checkout');
+  });
+
+closeConfirmation.addEventListener('click', event => {
+  location.reload();
 });
